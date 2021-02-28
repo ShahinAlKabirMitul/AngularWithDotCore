1b472e4e0594441300761e713e7b6133e3b471dd
Shahin Al Kabir Mitul <mitul.cse@gmail.com>
2021-02-28 15:42:38 +0600

    Add Login

--- DatingApp/DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/DatingApp/API/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -23,19 +25,50 @@ namespace API.Controllers
             this.dataContext = dataContext;
         }
 
-        [HttpPost]
-        public async Task<ActionResult<AppUser>> Register(string username,string password)
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegistrerDto registerDto)
         {
+            if (await UserExists(registerDto.UserName))
+            {
+                return BadRequest("UserName is already taken");
+            }
             using var hmc = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmc.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmc.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmc.Key
             };
              dataContext.Users.Add(user);
             await dataContext.SaveChangesAsync();
             return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await dataContext.Users.SingleOrDefaultAsync(s => s.UserName == loginDto.UserName.ToLower());
+            if (user == null)
+            {
+                return Unauthorized("Invlaid User Name");
+            }
+            using var hmc = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmc.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i]!= user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid Password ");
+                }
+            }
+
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await dataContext.Users.AnyAsync(s=>s.UserName== username);
         }
     }
 }
