26e68e6ee18d66315fd2d9addbd0e10bd4ee6b24
Shahin Al Kabir Mitul <mitul.cse@gmail.com>
2021-03-01 17:30:48 +0600

    Add Token

--- DatingApp/DatingApp/API/Startup.cs
+++ DatingApp/DatingApp/API/Startup.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +31,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
